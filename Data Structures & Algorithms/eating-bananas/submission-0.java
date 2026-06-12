e638bc6e31d139f91f65056091f90100fe89353e
lakiidev <77170483+lakiidev@users.noreply.github.com>
2026-06-12 16:25:00 +0200

    Add: eating-bananas - submission-0

---
+++ Data Structures & Algorithms/eating-bananas/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = 0;
+        for (int p : piles) high = Math.max(high, p);
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            int hours = 0;
+            for (int p : piles) hours += (p + mid - 1) / mid;
+            if (hours <= h)
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+}
