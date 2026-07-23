391a623cba7f55fc2866d345e9e309a81c7d5714
lakiidev <77170483+lakiidev@users.noreply.github.com>
2026-07-23 18:53:42 +0200

    Add: decode-ways - submission-0

---
+++ Data Structures & Algorithms/decode-ways/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        int[] dp = new int[n + 1];
+        dp[n] = 1;
+        dp[n - 1] = s.charAt(n - 1) != '0' ? 1 : 0;
+        dp[n - 1] = s.charAt(n - 1) != '0' ? 1 : 0;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (s.charAt(i) != '0') {
+                dp[i] += dp[i + 1];
+            }
+
+            int twoDigit = Integer.parseInt(s.substring(i, i + 2));
+            if (twoDigit >= 10 && twoDigit <= 26) {
+                dp[i] += dp[i + 2];
+            }
+        }
+        return dp[0];
+    }
+}
